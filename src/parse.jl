c1017af91b6d36651efe61903cae86e151a5e514
MasonGobat <masongobat@gmail.com>
2023-11-15 12:51:00 -0500

    added finding the document.xml and text in document

--- src/parse.jl
+++ src/parse.jl
@@ -5,7 +5,7 @@ filename = ""
 while (true)
     try
         print("Name of file to be parsed: ")
-        global filename = "./test/" *  readline()
+        global filename = "./test/" * readline()
         archive = open(filename, "r")
         break
     catch
@@ -13,5 +13,16 @@ while (true)
     end
 end
 
-files = ZipFile.Reader(filename)
-println(files)
+archive = ZipFile.Reader(filename)
+
+for f in archive.files
+    if (occursin("word/document.xml", f.name))
+        contents = read(f, String)
+        regex = r"<w:t>.*?</w:t>"
+        matches = eachmatch(regex, contents)
+
+        for str in matches
+            println(str)
+        end
+    end
+end
