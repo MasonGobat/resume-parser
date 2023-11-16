50272cf60cf85c769fa8f4659473e5eec064cfa9
MasonGobat <masongobat@gmail.com>
2023-11-15 21:31:33 -0500

    functionalized and grabbed just text from file

--- src/parse.jl
+++ src/parse.jl
@@ -1,28 +1,43 @@
 using ZipFile, Printf
 
-filename = ""
+function getFile()
+    filename = "./test/"
 
-while (true)
-    try
-        print("Name of file to be parsed: ")
-        global filename = "./test/" * readline()
-        archive = open(filename, "r")
-        break
-    catch
-        @printf "File: %s does not exist\n" filename
+    while (true)
+        try
+            print("Name of file to be parsed: ")
+            filename *= readline()
+            
+            if stat(filename).size == 0
+                throw(ArgumentError(""))
+            end
+
+            break
+        catch
+            @printf "File: %s does not exist\n" filename
+        end
     end
+
+    return filename
 end
 
-archive = ZipFile.Reader(filename)
+function getContents(filename::String)
+    archive = ZipFile.Reader(filename)
+    text = ""
 
-for f in archive.files
-    if (occursin("word/document.xml", f.name))
-        contents = read(f, String)
-        regex = r"<w:t>.*?</w:t>"
-        matches = eachmatch(regex, contents)
+    for f in archive.files
+        if (occursin("word/document.xml", f.name))
+            contents = read(f, String)
+            regex = r"<w:t>(.*?)</w:t>"
+            matches = eachmatch(regex, contents)
 
-        for str in matches
-            println(str)
+            for str in matches
+                text *= " " * str[1]
+            end
+
+            return text
         end
     end
 end
+
+println(getContents(getFile()))
